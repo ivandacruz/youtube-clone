bb680c0e5f46e02a90577a62b980f571c8f567a2
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2022-11-24 10:06:25 -0300

    add  components

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # youtube-clone
 
-## modern youtube clone
+
+## Project image
+<div align="center">
+        <img src="./public/001.png" width="100%" />
+
+</div>
+
+## Build
+- [React JS](https://pt-br.reactjs.org/)
+- [Rapid API](https://rapidapi.com/hub)
+- [Material UI 5](https://mui.com/pt/)
+
+## To run the application
+
+### installing dependencies
+- [npm](https://www.npmjs.com/)
+* $ npm install
+### run project
+* $ npm start
+
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState }  from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import ReactPlayer from 'react-player';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import { Videos, Loader } from "./";
-import { Link, useParams } from "react-router-dom"
-import { fetchFromAPI } from '../utils/fetchFromAPI';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-const  VideoDetail = () => {
+import { Videos, Loader } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
   const [videos, setVideos] = useState(null);
   const { id } = useParams();
@@ -14,27 +15,27 @@ const  VideoDetail = () => {
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
       .then((data) => setVideoDetail(data.items[0]))
-    
+
     fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
       .then((data) => setVideos(data.items))
   }, [id]);
 
   if(!videoDetail?.snippet) return <Loader />;
 
-  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = VideoDetail;
+  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetail;
 
   return (
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
-            <ReactPlayer url={`https://youtube.com/watch?v=${id}`} className="react-player" controls />
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
             </Typography>
-            <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff"}} py={1} px={2}>
+            <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
               <Link to={`/channel/${channelId}`}>
-                <Typography variant={{ sm: "subtitle1", md: "h6" }} color="white">
+                <Typography variant={{ sm: "subtitle1", md: 'h6' }}  color="#fff" >
                   {channelTitle}
                   <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
                 </Typography>
@@ -50,7 +51,7 @@ const  VideoDetail = () => {
             </Stack>
           </Box>
         </Box>
-        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center">
+        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
           <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
